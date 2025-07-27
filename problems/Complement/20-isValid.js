3e0e1789dfb4fe43074b60903355652b6a1a62b6
Aria He <rachelholic@163.com>
2025-07-27 23:56:15 +0800

    update

--- problems/Complement/20-isValid.js
+++ problems/Complement/20-isValid.js
@@ -21,6 +21,8 @@ var isValid = function(s) {
             stack.push(mapping.get(s[i]));  // 把对应的有括号给塞进栈
         } else if (stack.length === 0 || stack.pop() !== s[i] ) { // 栈为空或者栈顶元素不匹配当前字符，
         // 这里s[i]是有括号，和栈里的值对比
+        // stack.pop() 方法每次调用时都会移除并返回栈顶元素
+        // 无论后面的比较结果如何，stack.pop() 都会执行，一定会弹出栈顶元素
             return false;
         }
     }
