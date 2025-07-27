e8d6f55ca0d5f007dd463ea20b7b551e9e8c2331
Aria He <rachelholic@163.com>
2025-07-27 23:54:34 +0800

    update

--- problems/Complement/20-isValid.js
+++ problems/Complement/20-isValid.js
@@ -17,9 +17,10 @@ var isValid = function(s) {
     const stack = [];
 
     for(let i=0;i<s.length;i++) {
-        if(mapping.has(s[i])) {
-            stack.push(mapping.get(s[i]));
-        } else if (stack.length === 0 || stack.pop() !== s[i] ) { // 栈为空或者栈顶元素不匹配当前字符
+        if(mapping.has(s[i])) { // 是否有左括号
+            stack.push(mapping.get(s[i]));  // 把对应的有括号给塞进栈
+        } else if (stack.length === 0 || stack.pop() !== s[i] ) { // 栈为空或者栈顶元素不匹配当前字符，
+        // 这里s[i]是有括号，和栈里的值对比
             return false;
         }
     }
