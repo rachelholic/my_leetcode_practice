382af7c10cbddea803720adeafc7170426440a56
Aria He <rachelholic@163.com>
2025-08-08 13:05:57 +0800

    update

--- problems/1D_DP/322-coinChange.js
+++ problems/1D_DP/322-coinChange.js
@@ -12,8 +12,11 @@ var coinChange = function(coins, amount) {
     // 递推公式：dp[i] = Math.min(dp[i - coin] + 1, dp[i - coin] + 1, ...)
     // +1 表示 当前使用的这枚硬币，而 dp[i - coin] 表示 剩余金额所需的最少硬币数。两者相加就是使用当前硬币后的总硬币数。
 
+    // 创建一个数组来保存每个金额所需的最小硬币数
     const dp = new Array(amount+1).fill(Infinity);
+    // 初始条件：金额为0时需要0个硬币
     dp[0]=0;
+    // 尝试每一种硬币
     for(let coin of coins) { // 先遍历物品，也就是先放了一枚硬币，之后讨论其它放置情况
         for(let i=coin;i<=amount;i++) { // 再遍历背包容量，已经放了这枚coin，从这枚coin当前值开始计算
             // 更新dp[i]为min(当前值, 使用当前硬币后的值+1)
